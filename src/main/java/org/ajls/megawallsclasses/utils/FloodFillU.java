849bcfb6b4345dab73245721792a90303a6209ee
8439284 <your.email@example.com>
2025-10-30 13:38:58 +0800

    Respawn mechanic removed comments

--- src/main/java/org/ajls/megawallsclasses/utils/FloodFillU.java
+++ src/main/java/org/ajls/megawallsclasses/utils/FloodFillU.java
@@ -15,15 +15,6 @@ public class FloodFillU {
 
     public static HashSet<Location> floodFillRespawn(Location location, int distance) {
         HashSet<Vector> directions = new HashSet<>();
-//        for (int i = 0; i < 3; i++){
-//            for (int sign = 0; sign < 1; sign++) {
-//                int axis = i;
-//                if (sign == 1) {
-//                    axis = -  axis;
-//                }
-//                Vector vector = new Vector();
-//            }
-//        }
         directions.add(new Vector(-1 ,0, 0));
         directions.add(new Vector(1, 0, 0));
         directions.add(new Vector(0, -1, 0));
@@ -32,25 +23,17 @@ public class FloodFillU {
         directions.add(new Vector(0, 0, 1));
         HaxhMap<Location, Vector> currents = new HaxhMap<>();
         HaxhMap<Location, Vector> nexts = new HaxhMap<>();
-//        HaxhMap<Location, Vector> temp = new HaxhMap<>();
         HashSet<Location> returnSet = new HashSet<>();
+        HashSet<Location> checkedSet = new HashSet<>();
         currents.put(location, (Vector) null);
-        HaxhMap<Location, HashSet<Vector>> current2 = new HaxhMap<>();
         for (int i = 0; i < distance; i++) {
             for (Location currentLocation: currents.keySet()) {
                 HashSet<Vector> BirthDirections = currents.getValues(currentLocation, true);
-                HashSet<Vector> availableDirections = new HashSet<>();
-//                for (FloodFillDirection floodFillDirection: FloodFillDirection.values()) {
-//                                      if (!BirthDirections.contains(floodFillDirection)) {
-//                        availableDirections.add(floodFillDirection);
-//                    }
-//                }
                 for (Vector vector: directions) {
-                    if (!BirthDirections.contains(vector)) {
-                        //                        availableDirections.add(vector);
+                    if (!BirthDirections.contains(vector)) {  //!BirthDirections.contains(vector)  !checkedSet
                         Location next = currentLocation.clone().add(vector.multiply(-1));  //multiply -1 to make the goal vector , or just subtract vector
                         if (next.getBlock().getType() == Material.AIR) {  //todo make it check passable and also it would be good to avoid 1 block gap so only flood when there a 2 block gap so that steve can cross
-                            //
+                            //todo check if the original code makes people respawn on grass when using             int height = world.getHighestBlockYAt(teleportLocation);
 
                             nexts.put(next, vector.multiply(-1));
                         }
@@ -60,6 +43,7 @@ public class FloodFillU {
             }
 //            if (di) <= 16
             for (Location nextLocation: nexts.keySet()) {
+                checkedSet.add(nextLocation);
                 //if standable add to return set
                 if (nextLocation.clone().add(0, -1, 0).getBlock().getType() != Material.AIR){                    //TODO: make it standable instead of air, so a button will trick the plugin.(Actually get highest block also tricks it)
                     if (nextLocation.getBlock().getType() == Material.AIR) {
